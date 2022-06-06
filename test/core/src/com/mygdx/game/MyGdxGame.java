3d72296b22ac13d0f90a36a17912efec00a5a601
Lucas Guimarães <lucasmg09@hotmail.com>
2022-06-06 20:01:17 -0300

    update

--- test/core/src/com/mygdx/game/MyGdxGame.java
+++ test/core/src/com/mygdx/game/MyGdxGame.java
@@ -24,20 +24,23 @@ public class MyGdxGame extends ApplicationAdapter {
 	private Random numeroRandomico;
 	private float variacao = 0;
 	private float deltaTime;
-	private float velocidadeDeQueda = 0;
-	private float posicaoInicialVertical = 0;
+	private float velocidadeDeQueda[] = new float[10];
+	private float posicaoInicialVertical[] = new float[10];
+	private float decidaoBird[] = new float[10];
 	private float larguraDipositivo;
 	private float alturaDispositivo;
 	private float posicaoMovimentoCanoHorizontal;
 	private float espacoEntreCanos;
 	private float alturaentreOsCanosRandomica;
 	private ShapeRenderer shape;
-	private Circle passaroCirculo;
+	private Circle passaroCirculo[] = new Circle[10];
 	private Rectangle retanguloCanoTopo;
 	private Rectangle retanguloCanoBaixo;
 	private BitmapFont fonte;
 	private int pontuacao = 0;
 	private boolean marcouponto = false;
+	private boolean lifeBird[] = new boolean[3];
+	private int contBirdLife = 0;
 
 	private int estadoDoJogo = 0; // 0-> jogo não iniciado; 1->Jogo iniciado; 2->Game Over
 
@@ -64,7 +67,9 @@ public class MyGdxGame extends ApplicationAdapter {
 		gameOver = new Texture("game_over.png");
 
 		shape = new ShapeRenderer();
-		passaroCirculo = new Circle();
+		passaroCirculo[0] = new Circle();
+		passaroCirculo[1] = new Circle();
+		passaroCirculo[2] = new Circle();
 		retanguloCanoBaixo = new Rectangle();
 		retanguloCanoTopo = new Rectangle();
 
@@ -72,11 +77,21 @@ public class MyGdxGame extends ApplicationAdapter {
 		fonte.setColor(Color.YELLOW);
 		fonte.getData().setScale(6);
 
-		posicaoInicialVertical = Gdx.graphics.getHeight() / 2;
+		posicaoInicialVertical[0] = Gdx.graphics.getHeight() / 2;
+		posicaoInicialVertical[1] = Gdx.graphics.getHeight() / 2;
+		posicaoInicialVertical[2] = Gdx.graphics.getHeight() / 2;
+
+		lifeBird[0] = true;
+		lifeBird[1] = true;
+		lifeBird[2] = true;
+
 	}
 
 	@Override
 	public void render() {
+		// Quantidade de Birds jogando
+		contBirdLife = 3;
+
 		deltaTime = Gdx.graphics.getDeltaTime();
 		variacao += deltaTime * 20;
 		if (variacao > 2)
@@ -87,25 +102,54 @@ public class MyGdxGame extends ApplicationAdapter {
 		}
 
 		if (estadoDoJogo == 1) {
-			velocidadeDeQueda++;
-			posicaoInicialVertical = posicaoInicialVertical - velocidadeDeQueda;
+			velocidadeDeQueda[0]++;
+			velocidadeDeQueda[1]++;
+			velocidadeDeQueda[2]++;
+			posicaoInicialVertical[0] = posicaoInicialVertical[0] - velocidadeDeQueda[0];
+			posicaoInicialVertical[1] = posicaoInicialVertical[1] - velocidadeDeQueda[1];
+			posicaoInicialVertical[2] = posicaoInicialVertical[2] - velocidadeDeQueda[2];
 			posicaoMovimentoCanoHorizontal -= deltaTime * 200;
 
-			float randomCanoBaixo = alturaDispositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2
+			// Bird jogando sozinho
+			float alturaCanoTopo = alturaDispositivo / 2 + espacoEntreCanos / 2 + alturaentreOsCanosRandomica;
+			float alturaCanoBaixo = alturaDispositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2
 					+ alturaentreOsCanosRandomica;
-			float randomCanoTopo = alturaDispositivo / 2 + espacoEntreCanos / 2 + alturaentreOsCanosRandomica;
 
-			float vetorCanoBaixo = randomCanoBaixo;
-			float vetorCanoTopo = randomCanoTopo;
-			float decidaoBird = posicaoInicialVertical + 80;
-			
-			if (decidaoBird < vetorCanoTopo) {
-				velocidadeDeQueda = -2;
-			} else {
-				velocidadeDeQueda = +4;
+			// Bird 1 (Amarelo)
+			if (lifeBird[0] == true) {
+				decidaoBird[0] = posicaoInicialVertical[0] + 60;
+				if (decidaoBird[0] < alturaCanoTopo) {
+					velocidadeDeQueda[0] = -2;
+				} else {
+					velocidadeDeQueda[0] = +4;
+				}
 			}
 
-			// Verifica se os canos sumiram
+			// Bird 2 (Preto)
+			if (lifeBird[1] == true) {
+				decidaoBird[1] = posicaoInicialVertical[1] + 100;
+				if (decidaoBird[1] < alturaCanoTopo) {
+					velocidadeDeQueda[1] = -3;
+				} else {
+					if (alturaCanoBaixo <= -500 || alturaCanoTopo >= 550) {
+						velocidadeDeQueda[1] = +20;
+					}
+					velocidadeDeQueda[1] = +6;
+				}
+			}
+
+			// Bird 3 (Rosa)
+			if (lifeBird[2] == true) {
+				decidaoBird[2] = posicaoInicialVertical[2] + 100;
+				if (decidaoBird[2] < alturaCanoTopo) {
+					velocidadeDeQueda[2] = -5;
+				} else {
+					velocidadeDeQueda[2] = +5;
+				}
+
+			}
+
+			// Verificar se os canos sumiram
 			if (posicaoMovimentoCanoHorizontal < -canoTopo.getWidth()) {
 				posicaoMovimentoCanoHorizontal = larguraDipositivo;
 				alturaentreOsCanosRandomica = numeroRandomico.nextInt(400) - 200;
@@ -120,14 +164,20 @@ public class MyGdxGame extends ApplicationAdapter {
 			}
 		}
 
-		// Similar a um quadro em branco para ser desenhado
+		// Simulando
 		batch.begin();
+
 		batch.draw(fundo, 0, 0, larguraDipositivo, alturaDispositivo);
+
 		batch.draw(canoTopo, posicaoMovimentoCanoHorizontal,
 				alturaDispositivo / 2 + espacoEntreCanos / 2 + alturaentreOsCanosRandomica);
+
 		batch.draw(canoBaixo, posicaoMovimentoCanoHorizontal,
 				alturaDispositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2 + alturaentreOsCanosRandomica);
-		batch.draw(passaros[(int) variacao], 150, posicaoInicialVertical);
+
+		batch.draw(passaros[0], 150, posicaoInicialVertical[0]);
+		batch.draw(passaros[1], 150, posicaoInicialVertical[1]);
+		batch.draw(passaros[2], 150, posicaoInicialVertical[2]);
 
 		fonte.draw(batch, String.valueOf(pontuacao), larguraDipositivo / 2, alturaDispositivo - 100);
 
@@ -136,8 +186,20 @@ public class MyGdxGame extends ApplicationAdapter {
 		}
 		batch.end();
 
-		passaroCirculo.set(150 + passaros[0].getWidth() / 2, posicaoInicialVertical + passaros[0].getHeight() / 2,
+		// Bird 1
+		passaroCirculo[0].set(150 + passaros[0].getWidth() / 2,
+				posicaoInicialVertical[0] + passaros[0].getHeight() / 2,
 				passaros[0].getWidth() / 2);
+
+		// Bird 2
+		passaroCirculo[1].set(150 + passaros[1].getWidth() / 2,
+				posicaoInicialVertical[1] + passaros[1].getHeight() / 2,
+				passaros[1].getWidth() / 2);
+
+		// Bird 3
+		passaroCirculo[2].set(150 + passaros[1].getWidth() / 2,
+				posicaoInicialVertical[2] + passaros[2].getHeight() / 2,
+				passaros[1].getWidth() / 2);
 
 		retanguloCanoBaixo = new Rectangle(
 				posicaoMovimentoCanoHorizontal,
@@ -149,12 +211,40 @@ public class MyGdxGame extends ApplicationAdapter {
 				alturaDispositivo / 2 + espacoEntreCanos / 2 + alturaentreOsCanosRandomica,
 				canoTopo.getWidth(), canoTopo.getHeight());
 
-		// Teste de colisão
-		if (Intersector.overlaps(passaroCirculo, retanguloCanoBaixo)
-				|| Intersector.overlaps(passaroCirculo, retanguloCanoTopo) || posicaoInicialVertical <= 0
-				|| posicaoInicialVertical >= alturaDispositivo) {
+		// Teste de colisões dos passaros
+
+		// Bird 1
+		if (Intersector.overlaps(passaroCirculo[0], retanguloCanoBaixo)
+				|| Intersector.overlaps(passaroCirculo[0], retanguloCanoTopo) ||
+				posicaoInicialVertical[0] <= 0
+				|| posicaoInicialVertical[0] >= alturaDispositivo) {
+			lifeBird[0] = false;
+			contBirdLife--;
+		}
+
+		// Bird 2
+		if (Intersector.overlaps(passaroCirculo[1], retanguloCanoBaixo)
+				|| Intersector.overlaps(passaroCirculo[1], retanguloCanoTopo) ||
+				posicaoInicialVertical[1] <= 0
+				|| posicaoInicialVertical[1] >= alturaDispositivo) {
+			lifeBird[1] = false;
+			contBirdLife--;
+		}
+
+		// Bird 3
+		if (Intersector.overlaps(passaroCirculo[2], retanguloCanoBaixo)
+				|| Intersector.overlaps(passaroCirculo[2], retanguloCanoTopo) ||
+				posicaoInicialVertical[2] <= 0
+				|| posicaoInicialVertical[2] >= alturaDispositivo) {
+			lifeBird[2] = false;
+			contBirdLife--;
+		}
+
+		// Verificando a quantidade de birds vivos
+		if (contBirdLife == 0) {
 			estadoDoJogo = 2;
 		}
+
 	}
 
 	@Override
